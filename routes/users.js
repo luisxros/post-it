009753410d3bc4d9edfee7a80bff5bf5aa997f00
Luis Rosario <luisalmonter@gmail.com>
2020-11-15 17:28:47 -0500

    adds routes for users

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../controllers/users');
+
+
+router.get('/new', usersCtrl.new);
+
+module.exports = router;
