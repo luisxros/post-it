9db85874465c337c958745094344b0badfd96ff2
Luis Rosario <luisalmonter@gmail.com>
2020-11-15 21:31:10 -0500

    adds logout feature

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersCtrl = require('../controllers/users');
+const authorization = require('../utils/authorization');
 
 
 router.get('/new', usersCtrl.new);
@@ -10,5 +11,7 @@ router.post('/signup', usersCtrl.signUp);
 router.get('/signin', usersCtrl.signIn);
 
 router.post('/login', usersCtrl.login);
+
+
 
 module.exports = router;
