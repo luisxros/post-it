4ad58f5299108ff03c0439c441900b641200c69d
Luis Rosario <luisalmonter@gmail.com>
2020-11-20 00:21:30 -0500

    adds security

--- config/database.js
+++ config/database.js
@@ -1,13 +1,12 @@
 // require
 const mongoose = require('mongoose');
 
-
-const connectionURI = 'mongodb+srv://admin:abc1234@cluster0.hc47x.mongodb.net/post-it?retryWrites=true&w=majority'
-
 // shortcut to mongoose.connection object
 const db = mongoose.connection;
 
-mongoose.connect(connectionURI, {
+
+
+mongoose.connect(process.env.DATABASE_URI, {
    useNewUrlParser: true,
    useCreateIndex: true,
    useUnifiedTopology: true,
